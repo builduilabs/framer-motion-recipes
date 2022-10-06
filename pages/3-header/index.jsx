f4c0d872e2dc2b0bea677aae3ff93768bd83bf4c
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-06 12:02:29 -0400

    start of video

--- pages/3-header/index.jsx
+++ pages/3-header/index.jsx
@@ -1,90 +1,25 @@
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useScroll,
-  useTransform,
-} from "framer-motion";
-import { useRef } from "react";
-import { useEffect } from "react";
-
-function useScrollWithBound(bound, rest) {
-  let { scrollY } = useScroll(rest);
-  let scrollYBounded = useMotionValue(0);
-  let scrollYProgressBounded = useTransform(scrollYBounded, [0, bound], [0, 1]);
-
-  useEffect(() => {
-    return scrollY.onChange((latest) => {
-      let previous = scrollY.getPrevious();
-      let diff = latest - previous;
-      let currentPixelsScrolled = scrollYBounded.get();
-      let shouldGrow = diff > 0 && currentPixelsScrolled < bound;
-      let shouldShrink = diff < 0 && currentPixelsScrolled > 0;
-
-      if (shouldGrow || shouldShrink) {
-        scrollYBounded.set(currentPixelsScrolled + diff);
-      }
-    });
-  }, [bound, scrollYBounded, scrollY]);
-
-  return { scrollYBounded, scrollYProgressBounded };
-}
-
 export default function Header() {
-  let container = useRef();
-  let { scrollYBounded } = useScrollWithBound(600, {
-    container,
-    offset: ["start", "end"],
-  });
-
-  // Delay progress until 50% of the bound is scrolled
-  let scrollYProgressBounded = useTransform(
-    scrollYBounded,
-    [0, 500, 600],
-    [0, 0, 1]
-  );
-
   return (
     <div className="flex h-screen flex-col items-center justify-center overscroll-y-contain bg-gradient-to-br from-slate-700 to-slate-900 py-8 px-6 text-slate-600">
-      <div className="relative mx-auto flex w-full max-w-3xl flex-1 overflow-hidden rounded-2xl bg-white backdrop-blur-0">
-        <div ref={container} className="z-0 flex-1 overflow-y-scroll border-l">
-          <motion.header
-            style={{
-              height: useTransform(scrollYProgressBounded, [0, 1], [80, 50]),
-              backgroundColor: useMotionTemplate`rgba(255 255 255 / ${useTransform(
-                scrollYProgressBounded,
-                [0, 1],
-                [1, 0.1]
-              )})`,
-            }}
-            className="absolute inset-x-0 top-0 flex items-center justify-between px-8 shadow backdrop-blur-md"
-          >
-            <motion.p
-              style={{
-                scale: useTransform(scrollYProgressBounded, [0, 1], [1, 0.9]),
-              }}
-              className="flex origin-left items-center text-xl font-semibold uppercase"
-            >
+      <div className="relative mx-auto flex w-full max-w-3xl flex-1 overflow-hidden bg-white">
+        <div className="z-0 flex-1 overflow-y-scroll border-l">
+          <header className="flex h-20 items-center justify-between px-8 shadow">
+            <p className="flex origin-left items-center text-xl font-semibold uppercase">
               <span className="inline-block -rotate-90 text-[10px] leading-[0]">
                 The
               </span>{" "}
-              <span className="-ml-1 text-[24px] tracking-[-.075em]">
+              <span className="-ml-1 text-2xl tracking-[-.075em]">
                 Daily Bugle
               </span>
-            </motion.p>
-            <motion.nav
-              style={{
-                opacity: useTransform(scrollYProgressBounded, [0, 1], [1, 0]),
-              }}
-              className="flex space-x-4 text-xs font-medium text-slate-400"
-            >
+            </p>
+            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
-            </motion.nav>
-          </motion.header>
+            </nav>
+          </header>
 
-          <main className="px-8 pt-[120px]">
+          <main className="px-8 pt-12">
             <h1 className="h-10 w-4/5 rounded bg-slate-200 text-2xl font-bold" />
             <div className="mt-8 space-y-6">
               {[...Array(2).keys()].map((i) => (
--- pages/3-header/script.md
+++ pages/3-header/script.md
@@ -1,4 +1,6 @@
-Add sticky top-0 bg-white to header.
+## Sticky header
+
+To start, make the header sticky with CSS. Add sticky top-0 bg-white to header.
 
 ```jsx
 <header className="sticky top-0 flex items-center justify-between border-b p-8">
