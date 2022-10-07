91de013945fb56d47a355f6b4f3c82cbd08beccc
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-07 12:16:53 -0400

    begin of l3

--- pages/3-header/index.jsx
+++ pages/3-header/index.jsx
@@ -1,29 +1,8 @@
-import { motion, useMotionValue, useScroll } from "framer-motion";
-import { useEffect } from "react";
-
-let clamp = (value, min, max) => Math.min(Math.max(value, min), max);
-
 export default function Header() {
-  let { scrollY } = useScroll();
-  let height = useMotionValue(80);
-
-  useEffect(() => {
-    return scrollY.onChange((current) => {
-      let previous = scrollY.getPrevious();
-      let diff = current - previous;
-      let newHeight = height.get() - diff;
-
-      height.set(clamp(newHeight, 50, 80));
-    });
-  }, [height, scrollY]);
-
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
-        <motion.header
-          style={{ height }}
-          className="fixed inset-x-0 flex h-20 bg-white shadow"
-        >
+        <header className="flex h-20">
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
             <p className="flex origin-left items-center text-xl font-semibold uppercase">
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
@@ -39,9 +18,9 @@ export default function Header() {
               <a href="#">Culture</a>
             </nav>
           </div>
-        </motion.header>
+        </header>
 
-        <main className="px-8 pt-28">
+        <main className="px-8 pt-8">
           <h1 className="h-10 w-4/5 rounded bg-slate-200 text-2xl font-bold" />
           <div className="mt-8 space-y-6">
             {[...Array(2).keys()].map((i) => (
