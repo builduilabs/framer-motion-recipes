ab598f39dfd2f5dfc50c5b19efc9e4dafd3176e2
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-19 18:11:23 -0400

    Begin of video

--- pages/4-header-part-2/index.js
+++ pages/4-header-part-2/index.js
@@ -1,94 +1,41 @@
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useScroll,
-  useTransform,
-} from "framer-motion";
+import { motion, useMotionValue, useScroll } from "framer-motion";
 import { useEffect } from "react";
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
-function useBoundedScroll(bounds) {
+export default function Header() {
   let { scrollY } = useScroll();
-  let scrollYBounded = useMotionValue(0);
-  let scrollYBoundedProgress = useTransform(
-    scrollYBounded,
-    [0, bounds],
-    [0, 1]
-  );
+  let height = useMotionValue(80);
 
   useEffect(() => {
     return scrollY.onChange((current) => {
       let previous = scrollY.getPrevious();
       let diff = current - previous;
-      let newScrollYBounded = scrollYBounded.get() + diff;
+      let newHeight = height.get() - diff;
 
-      scrollYBounded.set(clamp(newScrollYBounded, 0, bounds));
+      height.set(Math.min(Math.max(newHeight, 50), 80));
     });
-  }, [bounds, scrollY, scrollYBounded]);
-
-  return { scrollYBounded, scrollYBoundedProgress };
-}
-
-export default function Header() {
-  let { scrollYBoundedProgress } = useBoundedScroll(500);
-  let scrollYBoundedProgressThrottled = useTransform(
-    scrollYBoundedProgress,
-    [0, 0.5, 1],
-    [0, 0, 1]
-  );
+  }, [height, scrollY]);
 
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
         <motion.header
-          style={{
-            height: useTransform(
-              scrollYBoundedProgressThrottled,
-              [0, 1],
-              [80, 50]
-            ),
-            backgroundColor: useMotionTemplate`rgb(255 255 255 / ${useTransform(
-              scrollYBoundedProgressThrottled,
-              [0, 1],
-              [1, 0.1]
-            )})`,
-          }}
-          className="fixed inset-x-0 flex h-20 shadow backdrop-blur-md"
+          style={{ height }}
+          className="fixed inset-x-0 flex h-20 bg-white shadow"
         >
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
-            <motion.p
-              style={{
-                scale: useTransform(
-                  scrollYBoundedProgressThrottled,
-                  [0, 1],
-                  [1, 0.9]
-                ),
-              }}
-              className="flex origin-left items-center text-xl font-semibold uppercase"
-            >
+            <p className="flex origin-left items-center text-xl font-semibold uppercase">
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
                 The
               </span>
               <span className="-ml-1 text-2xl tracking-[-.075em]">
                 Daily Bugle
               </span>
-            </motion.p>
-            <motion.nav
-              style={{
-                opacity: useTransform(
-                  scrollYBoundedProgressThrottled,
-                  [0, 1],
-                  [1, 0]
-                ),
-              }}
-              className="flex space-x-4 text-xs font-medium text-slate-400"
-            >
+            </p>
+            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
-            </motion.nav>
+            </nav>
           </div>
         </motion.header>
 
