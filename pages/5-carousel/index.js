11fa4da29ba8814bc10b93ffd9e668768203b056
Sam Selikoff <sam.selikoff@gmail.com>
2022-11-02 10:49:41 -0400

    wip

--- pages/5-carousel/index.js
+++ pages/5-carousel/index.js
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import useMeasure from "react-use-measure";
@@ -13,97 +13,112 @@ let images = [
 ];
 
 export default function Page() {
-  let [{ index, direction }, set] = useState({ index: 0, direction: null });
-  let [ref, bounds] = useMeasure();
+  let [index, setIndex] = useState(0);
 
   return (
-    <div className="flex h-full flex-col justify-center bg-black">
-      <div className="relative">
-        <motion.div
-          animate={{ x: `${index * -100}%` }}
-          transition={{
-            x: {
-              // type: "spring",
-              // bounce: 0.1,
-              // duration: 0.6,
-              // Original
-              type: "spring",
-              stiffness: 300,
-              damping: 30,
-              // Test
-              // duration: 0.45,
-              // ease: "easeInOut",
-            },
-            opacity: { duration: 0.2 },
-          }}
-          className="flex aspect-video w-full"
-          ref={ref}
-        >
-          {images.map((image, i) => (
-            <motion.img
-              key={image}
-              src={image}
-              // custom={direction}
-              // variants={{
-              //   enter: (direction) => ({
-              //     x: bounds.width * direction,
-              //     opacity: 0,
-              //   }),
-              //   center: { x: 0, opacity: 1 },
-              //   exit: (direction) => ({
-              //     x: -bounds.width * direction,
-              //     opacity: 0,
-              //   }),
+    <MotionConfig
+      transition={{
+        // x: { type: "spring", stiffness: 300, damping: 30 },
+        x: { type: "spring", bounce: 0, duration: 1 },
+        width: { type: "spring", bounce: 0, duration: 1 },
+        opacity: { duration: 0.2 },
+      }}
+    >
+      <div className="flex h-full flex-col justify-center bg-black">
+        <div className="relative overflow-hidden">
+          <motion.div
+            animate={{ x: `${index * -100}%` }}
+            // transition={{
+            //   x: {
+            //     type: "spring",
+            //     stiffness: 300,
+            //     damping: 30,
+            //   },
+            // }}
+            className="flex aspect-[3/2] w-full"
+          >
+            {images.map((image, i) => (
+              <motion.img
+                key={image}
+                src={image}
+                className="h-full w-full shrink-0 object-cover"
+                // transition={{
+                //   opacity: { duration: 0.2 },
+                // }}
+                animate={{ opacity: i === index ? 1 : 0 }}
+              />
+            ))}
+          </motion.div>
+
+          <AnimatePresence>
+            {index > 0 && (
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.7 }}
+                exit={{ opacity: 0, transition: "none" }}
+                // transition={{ opacity: { duration: 0.2 } }}
+                whileHover={{ opacity: 1 }}
+                className="absolute left-2 top-[calc(50%-16px)] flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                onClick={() => setIndex(index - 1)}
+              >
+                <ChevronLeftIcon className="h-6 w-6" />
+              </motion.button>
+            )}
+          </AnimatePresence>
+
+          <AnimatePresence>
+            {index + 1 < images.length && (
+              <motion.button
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.7 }}
+                exit={{ opacity: 0, transition: "none" }}
+                // transition={{ opacity: { duration: 0.2 } }}
+                whileHover={{ opacity: 1 }}
+                className="absolute right-2 top-[calc(50%-16px)] flex h-8 w-8 items-center justify-center rounded-full bg-white"
+                onClick={() => setIndex(index + 1)}
+              >
+                <ChevronRightIcon className="h-6 w-6" />
+              </motion.button>
+            )}
+          </AnimatePresence>
+
+          <div className="">
+            <motion.div
+              initial={false}
+              animate={{ x: `${-index * 50}%` }}
+              // transition={{
+              //   x: {
+              //     type: "spring",
+              //     stiffness: 300,
+              //     damping: 30,
+              //   },
               // }}
-              className="h-full w-full shrink-0 object-cover"
-              transition={{
-                // x: {
-                //   type: "spring",
-                //   stiffness: 300,
-                //   damping: 30,
-                // },
-                opacity: { duration: 0.2 },
-              }}
-              animate={{ opacity: i === index ? 1 : 0 }}
-              // initial="enter"
-              // animate="center"
-              // exit="exit"
-            />
-          ))}
-        </motion.div>
-
-        <AnimatePresence>
-          {index > 0 && (
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 0.7 }}
-              exit={{ opacity: 0, transition: "none" }}
-              transition={{ opacity: { duration: 0.2 } }}
-              whileHover={{ opacity: 1 }}
-              className="absolute left-2 top-[calc(50%-16px)] flex h-8 w-8 items-center justify-center rounded-full bg-white"
-              onClick={() => set({ index: index - 1, direction: -1 })}
+              className="mx-auto mt-8 flex aspect-[3/2] h-32 gap-1"
             >
-              <ChevronLeftIcon className="h-6 w-6" />
-            </motion.button>
-          )}
-        </AnimatePresence>
-
-        <AnimatePresence>
-          {index + 1 < images.length && (
-            <motion.button
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 0.7 }}
-              exit={{ opacity: 0, transition: "none" }}
-              transition={{ opacity: { duration: 0.2 } }}
-              whileHover={{ opacity: 1 }}
-              className="absolute right-2 top-[calc(50%-16px)] flex h-8 w-8 items-center justify-center rounded-full bg-white"
-              onClick={() => set({ index: index + 1, direction: 1 })}
-            >
-              <ChevronRightIcon className="h-6 w-6" />
-            </motion.button>
-          )}
-        </AnimatePresence>
+              {images.map((image, i) => (
+                <motion.button
+                  initial={false}
+                  animate={{ width: i === index ? "100%" : "50%" }}
+                  onClick={() => setIndex(i)}
+                  key={image}
+                  className="inline-block w-full shrink-0"
+                >
+                  <motion.img
+                    initial={false}
+                    animate={{ opacity: i === index ? 1 : 0.5 }}
+                    whileHover={{ opacity: 1 }}
+                    // transition={{
+                    //   opacity: { duration: 0.2 },
+                    // }}
+                    className="aspect-[3/2] h-full object-cover"
+                    src={image}
+                  />
+                </motion.button>
+              ))}
+            </motion.div>
+          </div>
+        </div>
       </div>
-    </div>
+    </MotionConfig>
   );
 }
