8e1d5436cc46aeb2442a867e991cca0a71b0f1cf
Sam Selikoff <sam.selikoff@gmail.com>
2022-11-07 16:01:24 -0500

    wip

--- pages/5-carousel-part-1/__end.js
+++ pages/5-carousel-part-1/__end.js
@@ -15,33 +15,26 @@ export default function Page() {
   let [index, setIndex] = useState(0);
 
   return (
-    <MotionConfig
-      transition={{
-        duration: 0.7,
-        ease: [0.32, 0.72, 0, 1],
-      }}
-    >
+    <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
       <div className="h-full bg-black">
         <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
           <div className="relative overflow-hidden">
             <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
-              {images.map((image, i) => (
-                <motion.img
-                  animate={{ opacity: i === index ? 1 : 0.3 }}
+              {images.map((image) => (
+                <img
                   key={image}
                   src={image}
                   className="aspect-[3/2] object-cover"
                 />
               ))}
             </motion.div>
-
             <AnimatePresence initial={false}>
               {index > 0 && (
                 <motion.button
                   initial={{ opacity: 0 }}
-                  animate={{ opacity: 0.6 }}
+                  animate={{ opacity: 0.7 }}
                   exit={{ opacity: 0, pointerEvents: "none" }}
-                  whileHover={{ opacity: 0.8 }}
+                  whileHover={{ opacity: 1 }}
                   className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
                   onClick={() => setIndex(index - 1)}
                 >
@@ -54,9 +47,9 @@ export default function Page() {
               {index + 1 < images.length && (
                 <motion.button
                   initial={{ opacity: 0 }}
-                  animate={{ opacity: 0.6 }}
+                  animate={{ opacity: 0.7 }}
                   exit={{ opacity: 0, pointerEvents: "none" }}
-                  whileHover={{ opacity: 0.8 }}
+                  whileHover={{ opacity: 1 }}
                   className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
                   onClick={() => setIndex(index + 1)}
                 >
---
+++ pages/6-carousel-part-2/script.md
@@ -0,0 +1,7 @@
+# Step
+
+Didn't do opacity
+
+```jsx
+<motion.img animate={{ opacity: i === index ? 1 : 0.3 }} />
+```
