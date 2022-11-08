5e6801b7fc45fcca49c0069fb01cb75e2f261320
Sam Selikoff <sam.selikoff@gmail.com>
2022-11-08 11:45:06 -0500

    wip

--- pages/6-carousel-part-2/index.js
+++ pages/6-carousel-part-2/index.js
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import useKeypress from "react-use-keypress";
 
 let images = [
   "/images/1.jpeg",
@@ -9,10 +10,45 @@ let images = [
   "/images/4.jpeg",
   "/images/5.jpeg",
   "/images/6.jpeg",
+  "/images/1.jpeg?",
+  "/images/2.jpeg?",
+  "/images/3.jpeg?",
+  "/images/4.jpeg?",
+  "/images/5.jpeg?",
+  "/images/6.jpeg?",
+  "/images/1.jpeg?1",
+  "/images/2.jpeg?1",
+  "/images/3.jpeg?1",
+  "/images/4.jpeg?1",
+  "/images/5.jpeg?1",
+  "/images/6.jpeg?1",
+  "/images/1.jpeg?2",
+  "/images/2.jpeg?2",
+  "/images/3.jpeg?2",
+  "/images/4.jpeg?2",
+  "/images/5.jpeg?2",
+  "/images/6.jpeg?2",
 ];
+
+let collapsedAspectRatio = 1 / 3;
+let fullAspectRatio = 3 / 2;
+let gap = 2;
+let margin = 12;
 
 export default function Page() {
   let [index, setIndex] = useState(0);
+
+  useKeypress("ArrowRight", () => {
+    if (index + 1 < images.length) {
+      setIndex(index + 1);
+    }
+  });
+
+  useKeypress("ArrowLeft", () => {
+    if (index > 0) {
+      setIndex((i) => i - 1);
+    }
+  });
 
   return (
     <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
@@ -20,10 +56,11 @@ export default function Page() {
         <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
           <div className="relative overflow-hidden">
             <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
-              {images.map((image) => (
-                <img
+              {images.map((image, i) => (
+                <motion.img
                   key={image}
                   src={image}
+                  animate={{ opacity: i === index ? 1 : 0.3 }}
                   className="aspect-[3/2] object-cover"
                 />
               ))}
@@ -59,20 +96,43 @@ export default function Page() {
             </AnimatePresence>
           </div>
 
-          <div className="absolute inset-x-0 bottom-6 overflow-hidden">
+          <div className="absolute inset-x-0 bottom-6 flex justify-center overflow-hidden">
             <motion.div
-              animate={{ x: `-${index * 18.66}px` }}
-              className="mx-auto flex aspect-[3/2] h-14 gap-8"
+              initial={false}
+              animate={{
+                x: `-${
+                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                  index * gap +
+                  margin
+                }%`,
+              }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+              className="flex h-14"
             >
               {images.map((image, i) => (
-                <motion.img
+                <motion.button
                   key={image}
-                  src={image}
-                  animate={{
-                    aspectRatio: i === index ? 3 / 2 : 1 / 3,
+                  onClick={() => setIndex(i)}
+                  whileHover={{ opacity: 1 }}
+                  initial={false}
+                  animate={i === index ? "active" : "inactive"}
+                  variants={{
+                    active: {
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1,
+                      aspectRatio: fullAspectRatio,
+                    },
+                    inactive: {
+                      marginLeft: "0%",
+                      marginRight: "0%",
+                      opacity: 0.5,
+                      aspectRatio: collapsedAspectRatio,
+                    },
                   }}
-                  className="object-cover"
-                />
+                >
+                  <motion.img src={image} className="h-full object-cover" />
+                </motion.button>
               ))}
             </motion.div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ let links = [
   { path: "/3-header-part-1", label: "Lesson 3 - Fixed header: Part 1" },
   { path: "/4-header-part-2", label: "Lesson 4 - Fixed header: Part 2" },
   { path: "/5-carousel-part-1", label: "Lesson 5 - Carousel: Part 1" },
+  { path: "/6-carousel-part-2", label: "Lesson 6 - Carousel: Part 2" },
 ];
 
 export default function HomePage() {
