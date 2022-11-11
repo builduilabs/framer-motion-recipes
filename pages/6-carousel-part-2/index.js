64461a1913c12d9529b0908b1a5219a1389bb1c7
Sam Selikoff <sam.selikoff@gmail.com>
2022-11-11 16:11:54 -0500

    End of Lesson 6

--- pages/6-carousel-part-2/index.js
+++ pages/6-carousel-part-2/index.js
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import useKeypress from "react-use-keypress";
 
 let images = [
   "/images/1.jpeg",
@@ -9,10 +10,45 @@ let images = [
   "/images/4.jpeg",
   "/images/5.jpeg",
   "/images/6.jpeg",
+  "/images/1.jpeg?1",
+  "/images/2.jpeg?1",
+  "/images/3.jpeg?1",
+  "/images/4.jpeg?1",
+  "/images/5.jpeg?1",
+  "/images/6.jpeg?1",
+  "/images/1.jpeg?2",
+  "/images/2.jpeg?2",
+  "/images/3.jpeg?2",
+  "/images/4.jpeg?2",
+  "/images/5.jpeg?2",
+  "/images/6.jpeg?2",
+  "/images/1.jpeg?3",
+  "/images/2.jpeg?3",
+  "/images/3.jpeg?3",
+  "/images/4.jpeg?3",
+  "/images/5.jpeg?3",
+  "/images/6.jpeg?3",
 ];
+
+let collapsedAspectRatio = 1 / 3;
+let fullAspectRatio = 3 / 2;
+let margin = 12;
+let gap = 2;
 
 export default function Page() {
   let [index, setIndex] = useState(0);
+
+  useKeypress("ArrowRight", () => {
+    if (index < images.length - 1) {
+      setIndex(index + 1);
+    }
+  });
+
+  useKeypress("ArrowLeft", () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  });
 
   return (
     <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
@@ -20,10 +56,11 @@ export default function Page() {
         <div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
           <div className="relative overflow-hidden">
             <motion.div animate={{ x: `-${index * 100}%` }} className="flex">
-              {images.map((image) => (
-                <img
+              {images.map((image, i) => (
+                <motion.img
                   key={image}
                   src={image}
+                  animate={{ opacity: i === index ? 1 : 0.3 }}
                   className="aspect-[3/2] object-cover"
                 />
               ))}
@@ -57,6 +94,51 @@ export default function Page() {
                 </motion.button>
               )}
             </AnimatePresence>
+          </div>
+
+          <div className="absolute inset-x-0 bottom-6 flex h-14 justify-center overflow-hidden">
+            <motion.div
+              initial={false}
+              animate={{
+                x: `-${
+                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                  margin +
+                  index * gap
+                }%`,
+              }}
+              style={{
+                aspectRatio: fullAspectRatio,
+                gap: `${gap}%`,
+              }}
+              className="flex"
+            >
+              {images.map((image, i) => (
+                <motion.button
+                  onClick={() => setIndex(i)}
+                  initial={false}
+                  whileHover={{ opacity: 1 }}
+                  animate={i === index ? "active" : "inactive"}
+                  variants={{
+                    active: {
+                      aspectRatio: fullAspectRatio,
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1,
+                    },
+                    inactive: {
+                      aspectRatio: collapsedAspectRatio,
+                      marginLeft: 0,
+                      marginRight: 0,
+                      opacity: 0.5,
+                    },
+                  }}
+                  className="shrink-0"
+                  key={image}
+                >
+                  <img src={image} className="h-full object-cover" />
+                </motion.button>
+              ))}
+            </motion.div>
           </div>
         </div>
       </div>
