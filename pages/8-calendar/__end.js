b05b089cfe8a480c7dfc550e0489aeb19ff5882d
Sam Selikoff <sam.selikoff@gmail.com>
2022-12-07 10:18:09 -0500

    wip

--- pages/8-calendar/__end.js
+++ pages/8-calendar/__end.js
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import {
   addMonths,
   eachDayOfInterval,
@@ -10,34 +11,35 @@ import {
   startOfWeek,
   subMonths,
 } from "date-fns";
-import { useState } from "react";
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import { useState } from "react";
 import useMeasure from "react-use-measure";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 
 export default function Page() {
   let [monthString, setMonthString] = useState(format(new Date(), "yyyy-MM"));
-  let [previousMonthString, setPreviousMonthString] = useState(monthString);
+  let [direction, setDirection] = useState();
   let [isAnimating, setIsAnimating] = useState(false);
   let month = parse(monthString, "yyyy-MM", new Date());
 
   function nextMonth() {
-    if (!isAnimating) {
-      let nextDate = addMonths(parse(monthString, "yyyy-MM", new Date()), 1);
-      setPreviousMonthString(monthString);
-      setMonthString(format(nextDate, "yyyy-MM"));
-    }
+    if (isAnimating) return;
+
+    let next = addMonths(month, 1);
+
+    setMonthString(format(next, "yyyy-MM"));
+    setDirection(1);
+    setIsAnimating(true);
   }
 
-  function prevMonth() {
-    if (!isAnimating) {
-      let prevDate = subMonths(parse(monthString, "yyyy-MM", new Date()), 1);
-      setPreviousMonthString(monthString);
-      setMonthString(format(prevDate, "yyyy-MM"));
-    }
-  }
+  function previousMonth() {
+    if (isAnimating) return;
 
-  let direction = monthString > previousMonthString ? 1 : -1;
+    let previous = subMonths(month, 1);
+
+    setMonthString(format(previous, "yyyy-MM"));
+    setDirection(-1);
+    setIsAnimating(true);
+  }
 
   let days = eachDayOfInterval({
     start: startOfWeek(startOfMonth(month)),
@@ -46,54 +48,59 @@ export default function Page() {
 
   return (
     <MotionConfig transition={transition}>
-      <div className="flex min-h-screen  items-start bg-stone-800 pt-16">
+      <div className="flex min-h-screen items-start bg-stone-800 pt-16 text-stone-900">
         <div className="relative mx-auto w-full max-w-md overflow-hidden rounded-2xl bg-white">
-          <ResizablePanel>
-            <AnimatePresence
-              mode="popLayout"
-              initial={false}
-              custom={{ direction }}
-            >
-              <motion.div
-                key={monthString}
-                onAnimationStart={() => setIsAnimating(true)}
-                onAnimationComplete={() => setIsAnimating(false)}
-                initial="enter"
-                animate="center"
-                exit="exit"
-              >
-                <div className="py-8">
-                  <div className="flex flex-col justify-center rounded text-center">
+          <div className="py-8">
+            <div className="flex flex-col justify-center rounded text-center">
+              <ResizablePanel>
+                <AnimatePresence
+                  mode="popLayout"
+                  initial={false}
+                  custom={direction}
+                  onExitComplete={() => setIsAnimating(false)}
+                >
+                  <motion.div
+                    key={monthString}
+                    initial="enter"
+                    animate="middle"
+                    exit="exit"
+                  >
                     <header className="relative flex justify-between px-8">
-                      <button
-                        className="relative z-10 flex items-center justify-center rounded-full p-1.5 hover:bg-stone-100"
-                        onClick={prevMonth}
+                      <motion.button
+                        variants={removeImmediately}
+                        className="z-10 rounded-full p-1.5 hover:bg-stone-100"
+                        onClick={previousMonth}
                       >
                         <ChevronLeftIcon className="h-4 w-4" />
-                      </button>
+                      </motion.button>
                       <motion.p
-                        custom={{ direction }}
                         variants={variants}
+                        custom={direction}
                         className="absolute inset-0 flex items-center justify-center font-semibold"
                       >
                         {format(month, "MMMM yyyy")}
                       </motion.p>
-                      <button
-                        className="relative z-10 flex items-center justify-center rounded-full p-1.5 hover:bg-stone-100"
+                      <motion.button
+                        variants={removeImmediately}
+                        className="z-10 rounded-full p-1.5 hover:bg-stone-100"
                         onClick={nextMonth}
                       >
                         <ChevronRightIcon className="h-4 w-4" />
-                      </button>
-                      <div
+                      </motion.button>
+                      <motion.div
                         className="absolute inset-0"
                         style={{
                           backgroundImage:
-                            "linear-gradient(to right, white 10%, transparent, white 90%)",
+                            "linear-gradient(to right, white 15%, transparent 30%, transparent 70%, white 85%)",
                         }}
-                      ></div>
+                        variants={removeImmediately}
+                      />
                     </header>
 
-                    <div className="mt-6 grid grid-cols-7 gap-x-2 gap-y-6 px-8 text-sm">
+                    <motion.div
+                      variants={removeImmediately}
+                      className="mt-6 grid grid-cols-7 gap-y-6 px-8 text-sm"
+                    >
                       <span className="font-medium text-stone-500">Su</span>
                       <span className="font-medium text-stone-500">Mo</span>
                       <span className="font-medium text-stone-500">Tu</span>
@@ -101,61 +108,57 @@ export default function Page() {
                       <span className="font-medium text-stone-500">Th</span>
                       <span className="font-medium text-stone-500">Fr</span>
                       <span className="font-medium text-stone-500">Sa</span>
-                    </div>
+                    </motion.div>
 
                     <motion.div
-                      custom={{ direction }}
                       variants={variants}
-                      className="mt-4 grid grid-cols-7 gap-x-2 gap-y-6 px-8 text-sm"
+                      custom={direction}
+                      className="mt-6 grid grid-cols-7 gap-y-6 px-8 text-sm"
                     >
                       {days.map((day) => (
                         <span
-                          key={format(day, "yyyy-MM-dd")}
                           className={`${
-                            isSameMonth(day, month)
-                              ? "text-stone-800"
-                              : "text-stone-300"
+                            isSameMonth(day, month) ? "" : "text-stone-300"
                           } font-semibold`}
+                          key={format(day, "yyyy-MM-dd")}
                         >
                           {format(day, "d")}
                         </span>
                       ))}
                     </motion.div>
-                  </div>
-                </div>
-              </motion.div>
-            </AnimatePresence>
-          </ResizablePanel>
+                  </motion.div>
+                </AnimatePresence>
+              </ResizablePanel>
+            </div>
+          </div>
         </div>
       </div>
     </MotionConfig>
   );
 }
-let transition = { type: "tween", ease: "easeInOut", duration: 0.3 };
-// let transition = { type: "spring", bounce: 0, duration: 0.3 };
+
+// let transition = { type: "tween", duration: 0.25 };
+let transition = { type: "spring", bounce: 0, duration: 0.3 };
 let variants = {
-  enter: ({ direction }) => ({
-    opacity: 0,
-    x: `${direction * 100}%`,
-  }),
-  center: {
-    x: `0%`,
-    // opacity: [0, 1, 1],
-    opacity: 1,
-    transition: {
-      ...transition,
-      opacity: { ...transition, times: [0, 0.75, 1] },
-    },
+  enter: (direction) => {
+    // console.log({ direction });
+    return { x: `${100 * direction}%`, opacity: 0 };
+    // return { x: "100%" };
   },
-  exit: ({ direction }) => ({
-    opacity: 0,
-    // opacity: [1, 0, 0],
-    x: `${-direction * 100}%`,
-    transition: {
-      ...transition,
-      opacity: { ...transition, times: [0, 0.25, 1] },
-    },
-  }),
+  middle: { x: "0%", opacity: 1 },
+  // exit: { x: "-100%" },
+  exit: (direction) => {
+    // return { x: "-100%" };
+    return { x: `${-100 * direction}%`, opacity: 0 };
+  },
+  // exit: (direction) => {
+  //   console.log({ direction });
+  //   return { x: `${-100 * direction}%` };
+  // },
+};
+
+let removeImmediately = {
+  exit: { visibility: "hidden" },
 };
 
 function ResizablePanel({ children }) {
